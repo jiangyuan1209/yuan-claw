a5027f13fc7be45072dea62a1e15e1d1c91125ca
蒋远 <2826738157@qq.com>
2026-06-13 17:38:02 +0800

    feat: add CI workflow auto publish to npm & GH Packages

--- idea-plugin/src/main/kotlin/com/jiangyuan/yuanclaw/YuanClawIcons.kt
+++
@@ -1,9 +0,0 @@
-package com.jiangyuan.yuanclaw
-
-import com.intellij.openapi.util.IconLoader
-import javax.swing.Icon
-
-object YuanClawIcons {
-    @JvmField
-    val Toolbar: Icon = IconLoader.getIcon("/icons/yuan-claw.svg", javaClass)
-}
